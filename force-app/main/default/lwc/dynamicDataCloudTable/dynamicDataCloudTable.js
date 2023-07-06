ef37bfad8ae416e74ef9d5682cfce5eab5d71e09
Luis Zerba <lcorrea@lcorrea-ltmb9nn.internal.salesforce.com>
2023-07-06 19:41:36 -0300

    Dynamic DataCloud Component

---
+++ force-app/main/default/lwc/dynamicDataCloudTable/dynamicDataCloudTable.js
@@ -0,0 +1,37 @@
+import { LightningElement } from 'lwc';
+import { api,wire,track } from 'lwc';
+import getEngagementListFromContactByAPI from '@salesforce/apex/DF_EngagementAPIUtil.getEngagementListFromContactByAPI'
+
+const columns = [
+    { label: '#Id', fieldName: 'transaction_id__c', type: 'text'},
+    { label: 'CPF', fieldName: 'cpf__c', type: 'text'},
+    { label: 'Valor', fieldName: 'value__c', type: 'currency' },
+    { label: 'Categoria', fieldName: 'purchase_category__c', type: 'text'}
+];
+
+export default class DynamicDataCloudTable extends LightningElement {
+
+    @api
+    propertyDMO;
+
+    @track data;
+    columns = columns;
+
+    @api recordId;
+    @track error;
+
+    @wire(getEngagementListFromContactByAPI, {sourceRecordId: '$recordId'} )
+    engagementData({error, data}){
+        if (data) {
+            this.data = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.data = undefined;
+            console.error('[error]:', error);
+        }
+    }
+
+
+
+}
